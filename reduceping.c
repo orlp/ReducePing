60bd3565d7ca8b692e9dc104b5a4d37ea4032d4b
Orson Peters <spionman007@gmail.com>
2012-03-11 23:38:43 +0100

    lots of work

--- reduceping.c
+++ reduceping.c
@@ -1,75 +1,299 @@
 #include <windows.h>
+#include <stdlib.h>
 
-const char g_szClassName[] = "myWindowClass";
+#include "resource.h"
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch(msg)
-    {
+/* menu item constants */
+#define BTN_DISABLE_REDUCEPING 120
+#define BTN_ENABLE_REDUCEPING 121
+#define TEXT_REDUCEPING_ENABLED 122
+
+/* other constants */
+const char ROOT_KEY[] = "SYSTEM\\CurrentControlSet\\services\\Tcpip\\Parameters\\Interfaces\\";
+
+int is_reduceping_enabled() {
+    HKEY intkey;
+    
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, ROOT_KEY, 0, KEY_READ, &intkey) != ERROR_SUCCESS) {
+        MessageBox(NULL, "ReducePing can't find any network interface keys in the registry and can not continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+        exit(1);
+    }
+    
+    DWORD numsubkeys;
+    if (RegQueryInfoKey(intkey, NULL, NULL, NULL, &numsubkeys, NULL, NULL, NULL, NULL, NULL, NULL, NULL) != ERROR_SUCCESS) {
+        MessageBox(NULL, "ReducePing has encountered an error during querying the amount of sub keys and can not continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+        RegCloseKey(intkey);
+        exit(1);
+    }
+    
+    long i;
+    long num_tcp_ack_keys = 0;
+    for (i = 0; i < numsubkeys; i++) {
+        char subkey[256 + sizeof(ROOT_KEY)];
+        
+        strncpy(subkey, ROOT_KEY, strlen(ROOT_KEY));
+        
+        if (RegEnumKey(intkey, i, subkey + strlen(ROOT_KEY), sizeof(subkey)) != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing has encountered an error during enumerating registry keys, attempting to continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            continue;
+        }
+        
+        HKEY tcpkey;
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, subkey, 0, KEY_READ, &tcpkey) != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing encountered an error while reading a key (this should never happen). Attempting to continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            RegCloseKey(intkey);
+            continue;
+        }
+        
+        DWORD value;
+        DWORD size = sizeof(value);
+        DWORD type = REG_DWORD;
+        if (RegQueryValueEx(tcpkey, "TcpAckFrequency", NULL, &type, (BYTE *) &value, &size) == ERROR_SUCCESS) {
+            if (value == 1) {
+                num_tcp_ack_keys++;
+            }
+        }
+        
+        RegCloseKey(tcpkey);
+    }
+    
+    RegCloseKey(intkey);
+    
+    if (num_tcp_ack_keys == 0) {
+        return 0;
+    } else if (num_tcp_ack_keys == i) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+void enable_reduceping() {
+    HKEY intkey;
+    
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, ROOT_KEY, 0, KEY_READ, &intkey) != ERROR_SUCCESS) {
+        MessageBox(NULL, "ReducePing can't find any network interface keys in the registry and can not continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+        exit(1);
+    }
+    
+    DWORD numsubkeys;
+    if (RegQueryInfoKey(intkey, NULL, NULL, NULL, &numsubkeys, NULL, NULL, NULL, NULL, NULL, NULL, NULL) != ERROR_SUCCESS) {
+        MessageBox(NULL, "ReducePing has encountered an error during querying the amount of sub keys and can not continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+        RegCloseKey(intkey);
+        exit(1);
+    }
+    
+    long i;
+    for (i = 0; i < numsubkeys; i++) {
+        char subkey[256 + sizeof(ROOT_KEY)];
+        
+        strncpy(subkey, ROOT_KEY, strlen(ROOT_KEY));
+        
+        if (RegEnumKey(intkey, i, subkey + strlen(ROOT_KEY), sizeof(subkey)) != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing has encountered an error during enumerating registry keys, attempting to continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            continue;
+        }
+        
+        HKEY tcpkey;
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, subkey, 0, KEY_WRITE, &tcpkey) != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing can't write to the registry, make sure that your Windows account has Administrator rights.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            RegCloseKey(intkey);
+            return;
+        }
+        
+        const DWORD value = 1;
+        if (RegSetValueEx(tcpkey, "TcpAckFrequency", 0, REG_DWORD, (const BYTE *) &value, sizeof(value)) != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing encountered an error while setting the TcpAckFrequency registry value, attempting to continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            RegCloseKey(tcpkey);
+            continue;
+        }
+        
+        RegCloseKey(tcpkey);
+    }
+    
+    RegCloseKey(intkey);
+}
+
+void disable_reduceping() {
+    HKEY intkey;
+    
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, ROOT_KEY, 0, KEY_READ, &intkey) != ERROR_SUCCESS) {
+        MessageBox(NULL, "ReducePing can't find any network interface keys in the registry and can not continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+        exit(1);
+    }
+    
+    DWORD numsubkeys;
+    if (RegQueryInfoKey(intkey, NULL, NULL, NULL, &numsubkeys, NULL, NULL, NULL, NULL, NULL, NULL, NULL) != ERROR_SUCCESS) {
+        MessageBox(NULL, "ReducePing has encountered an error during querying the amount of sub keys and can not continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+        RegCloseKey(intkey);
+        exit(1);
+    }
+    
+    long i;
+    for (i = 0; i < numsubkeys; i++) {
+        char subkey[256 + sizeof(ROOT_KEY)];
+        
+        strncpy(subkey, ROOT_KEY, strlen(ROOT_KEY));
+        
+        if (RegEnumKey(intkey, i, subkey + strlen(ROOT_KEY), sizeof(subkey)) != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing has encountered an error during enumerating registry keys, attempting to continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            continue;
+        }
+        
+        HKEY tcpkey;
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, subkey, 0, KEY_WRITE, &tcpkey) != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing can't write to the registry, make sure that your Windows account has Administrator rights.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            RegCloseKey(intkey);
+            return;
+        }
+        
+        if (RegDeleteValue(tcpkey, "TcpAckFrequency") != ERROR_SUCCESS) {
+            MessageBox(NULL, "ReducePing encountered an error while deleting the TcpAckFrequency registry value, attempting to continue.", "ReducePing", MB_ICONEXCLAMATION | MB_OK);
+            RegCloseKey(tcpkey);
+            continue;
+        }
+        
+        RegCloseKey(tcpkey);
+    }
+    
+    RegCloseKey(intkey);
+}
+
+void update_ui(HWND hWnd) {
+    switch (is_reduceping_enabled()) {
+    case 0:
+        EnableWindow(GetDlgItem(hWnd, BTN_DISABLE_REDUCEPING), FALSE);
+        EnableWindow(GetDlgItem(hWnd, BTN_ENABLE_REDUCEPING), TRUE);
+        SetDlgItemText(hWnd, TEXT_REDUCEPING_ENABLED, "Disabled");
+        break;
+    case 1:
+        EnableWindow(GetDlgItem(hWnd, BTN_ENABLE_REDUCEPING), FALSE);
+        EnableWindow(GetDlgItem(hWnd, BTN_DISABLE_REDUCEPING), TRUE);
+        SetDlgItemText(hWnd, TEXT_REDUCEPING_ENABLED, "Enabled");
+        break;
+    case -1:
+        EnableWindow(GetDlgItem(hWnd, BTN_ENABLE_REDUCEPING), TRUE);
+        EnableWindow(GetDlgItem(hWnd, BTN_DISABLE_REDUCEPING), TRUE);
+        SetDlgItemText(hWnd, TEXT_REDUCEPING_ENABLED, "Partially enabled");
+        break;
+    }
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch(msg) {
+        case WM_COMMAND:
+            if (((HWND)lParam) && (HIWORD(wParam) == BN_CLICKED)) {
+                int btn = LOWORD(wParam);
+                
+                switch (btn) {
+                case BTN_DISABLE_REDUCEPING:
+                    disable_reduceping();
+                    update_ui(hwnd);
+                    break;
+                case BTN_ENABLE_REDUCEPING:
+                    enable_reduceping();
+                    update_ui(hwnd);
+                    break;
+                }
+            }
+            break;
         case WM_CLOSE:
             DestroyWindow(hwnd);
-        break;
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);
-        break;
+            break;
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
+    
     return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    /* start with creating our application's default fonts */
+    HFONT default_font;
+    HFONT bold_font;
+    
+    NONCLIENTMETRICS nonclientmetrics;
+    nonclientmetrics.cbSize = sizeof(NONCLIENTMETRICS);
+    
+    if (!SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS), &nonclientmetrics, 0)) {
+        MessageBox(NULL, "Error while retrieving default font", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 1;
+    }
+    
+    LOGFONT bold_logfont = nonclientmetrics.lfMessageFont;
+    bold_logfont.lfWeight = 700;
+    bold_logfont.lfHeight *= 1.1;
+    
+    default_font = CreateFontIndirect(&nonclientmetrics.lfMessageFont);
+    bold_font = CreateFontIndirect(&bold_logfont);
+    
+    if (!default_font || !bold_font) {
+        MessageBox(NULL, "Error while retrieving default font", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 1;
+    }
+
+    /* register window class */
     WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Msg;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2));
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) COLOR_WINDOW;
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = "ReducePingClass";
+    wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
 
-    //Step 1: Registering the Window Class
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.style         = 0;
-    wc.lpfnWndProc   = WndProc;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hInstance     = hInstance;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    wc.lpszMenuName  = NULL;
-    wc.lpszClassName = g_szClassName;
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
-
-    if(!RegisterClassEx(&wc))
-    {
-        MessageBox(NULL, "Window Registration Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
-    // Step 2: Creating the Window
-    hwnd = CreateWindowEx(
-        0,
-        g_szClassName,
-        "ReducePing",
-        WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
-        NULL, NULL, hInstance, NULL);
+    /* create window */
+    HWND hwnd = CreateWindowEx(0, "ReducePingClass", "ReducePing", WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 398, 260, NULL, NULL, hInstance, NULL);
 
-    if(hwnd == NULL)
-    {
-        MessageBox(NULL, "Window Creation Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
+    if (!hwnd) {
+        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
+    
+    /* create controls */
+    HWND text = CreateWindow("static", "This is ReducePing, a small utility to tune the \"TCPAckFrequency\" setting of Windows, which can drastically improve latency for some applications (mainly games) at the cost of slightly less throughput.\n\nBy default Windows waits with replying to a server so it can combine replies into one. This improves bandwidth, at the cost of latency. ReducePing disables this feature, so your latency is improved at the cost of a bit less bandwidth.\n\nAfter enabling/disabling ReducePing a restart is required for the change to take effect.",  WS_CHILD | WS_VISIBLE | SS_LEFT, 10, 10, 370, 280,  hwnd, NULL, hInstance, NULL);
+    
+    HWND disable_reduceping = CreateWindowEx(0, "BUTTON", "Disable ReducePing", WS_VISIBLE |  WS_CHILD |  BS_PUSHBUTTON, 10, 260 - 10 - 60, 115, 30, hwnd, (HMENU)BTN_DISABLE_REDUCEPING, hInstance, NULL);
+    
+    HWND enable_reduceping = CreateWindowEx(0, "BUTTON", "Enable ReducePing", WS_VISIBLE |  WS_CHILD |  BS_PUSHBUTTON, 400 - 115 - 20, 260 - 10 - 60, 115, 30, hwnd, (HMENU)BTN_ENABLE_REDUCEPING, hInstance, NULL);
+    
+    HWND reduceping_enabled = CreateWindow("static", "Enabled", WS_CHILD | WS_VISIBLE | SS_CENTER, 398 / 2 - 130 / 2, 260 - 10 - 40 - 15, 130, 20, hwnd, (HMENU) TEXT_REDUCEPING_ENABLED, hInstance, NULL);
+    
+    /* set fonts */
+    SendMessage(text, WM_SETFONT, (WPARAM) default_font, MAKELPARAM(TRUE, 0));
+    SendMessage(disable_reduceping, WM_SETFONT, (WPARAM) default_font, MAKELPARAM(TRUE, 0));
+    SendMessage(enable_reduceping, WM_SETFONT, (WPARAM) default_font, MAKELPARAM(TRUE, 0));
+    SendMessage(reduceping_enabled, WM_SETFONT, (WPARAM) bold_font, MAKELPARAM(TRUE, 0));
+    
+    /* update the UI */
+    update_ui(hwnd);
 
+    /* show window and start update loop */
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    // Step 3: The Message Loop
-    while(GetMessage(&Msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-    return Msg.wParam;
+    
+    /* clean up */
+    DeleteObject(default_font);
+    DeleteObject(bold_font);
+    
+    return msg.wParam;
 }
--- resource.h
+++ resource.h
@@ -5,3 +5,6 @@
 #define IDI_ICON1                               106
 #define IDI_ICON2                               107
 #define IDI_ICON3                               108
+#define IDD_DIALOG1                             110
+#define IDC_BUTTON2                             1002
+#define IDC_BUTTON1                             1003
